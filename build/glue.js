22ee86e07128fba178642b2480d208b5fad1e7ef
Hernan Zhou <hernanzh@gmail.com>
2014-05-29 20:10:52 +0200

    update build

--- build/glue.js
+++ build/glue.js
@@ -6449,11 +6449,15 @@ glue.module.create(
 
                         context.restore();
                     },
+                    pointerEvents: true,
                     pointerDown: function (e) {
                         var i,
                             l = children.length,
                             childEvent,
                             pos;
+                        if (!this.pointerEvents) {
+                            return;
+                        }
                         callRegistrants('pointerDown', e);
 
                         if (l) {
@@ -6472,6 +6476,9 @@ glue.module.create(
                             l = children.length,
                             childEvent,
                             pos;
+                        if (!this.pointerEvents) {
+                            return;
+                        }
                         callRegistrants('pointerMove', e);
 
                         if (l) {
@@ -6490,6 +6497,9 @@ glue.module.create(
                             l = children.length,
                             childEvent,
                             pos;
+                        if (!this.pointerEvents) {
+                            return;
+                        }
                         callRegistrants('pointerUp', e);
 
                         if (l) {
